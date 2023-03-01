64f26689ebf76f30bdf076728404b495f8308774
kyle <kylehippledev@gmail.com>
2023-03-01 16:56:07 -0600

    fixed value for office number not displaying implementing to intern and engineer

--- index.js
+++ index.js
@@ -4,51 +4,115 @@ const fs = require('fs');
 
 //Employee Classes
 const Manager = require('./lib/Manager');
-
 const Employee = require('./lib/Employee');
-
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const member = [];
 
 //Questions Function for inquirer
+//many maneger questions lol thought it was funny 
+const manyQuestions = async () => {
+    const answersManeger = await inquirer.prompt([
+        { 
+            type: 'input',
+            name: 'name',
+            message: 'Manager name?',
+        },
+        { 
+            type: 'input',
+            name: 'id',
+            message: 'Manager id?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Manager email?' ,
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Manager office number?' ,
+        },
+    ]);
+    const managerClassCreation = async (DATA) => {
+        const { name, id,  email, officeNumber} = DATA;
+        console.log(answersManeger)
+        const manager = new Manager(name, id, email, officeNumber);
+        return manager;
+    }
+    managerClassCreation(answersManeger);
+    member.push(answersManeger);
+    await addMember();
+}
 
-const questions = async () => {
-    const answers = await inquirer.prompt([
+const addMember = async () => {
+    const answersChoice = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'Add another member?',
+            choices: ['Engineer', 'Intern', 'No']
+        },
+    ]);
+    if (answersChoice.addMember === 'Engineer') {
+        await engineerQuestions();
+    }
+    if (answersChoice.addMember === 'Intern') {
+        await internQuestions();
+    }
+    if (answersChoice.addMember === 'No') {
+        return
+    }
+}
+
+const engineerQuestions = async () => {
+    const answersEngineer = await inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message:   'Employee name?',
+            message: 'Employee name?',
         },
-        {
-            type: 'input',
-            name: 'id' ,  
-            message: 'Employee id?',
-        },
-
         {
             type: 'input',
             name: 'email',
             message: 'Employees email?' ,
         },
         {
-            type: 'list',
-            name: 'role',
-            message: 'Employee Role?',
-            choices: ['Engineer', 'Intern',    'Manager']
-        }
+            type: 'input',
+            name: 'github',
+            message: 'Employees GitHub?' ,
+        },
     ]);
-    ///make models and fisll with somethingg to sorts managers interns out
-    // manager needs office number get role to be overwritten
-    // engineer neefs github gets role to be overwrittes
-    //  i ntern needs  school get ro le to be overwri tten
-    return answers;
+    const newEngineer = new Engineer(answersEngineer.name, answersEngineer.email, answersEngineer.github);
+    member.push(newEngineer);
+    await addMember();
 }
-//check if inquirer is working
+
+const internQuestions = async () => {
+    const answersIntern = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Employee name?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Employees email?' ,
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Employees school?' ,
+        },
+    ]);
+    const newIntern = new Intern(answersIntern.name, answersIntern.email, answersIntern.school);
+    member.push(newIntern);
+    await addMember();
+}
+
 const init = async () => {
-    const answers = await questions();
-    console.log(answers);
+    await manyQuestions();
+    console.log (member);
 }
-
-
-//init function
 init();
